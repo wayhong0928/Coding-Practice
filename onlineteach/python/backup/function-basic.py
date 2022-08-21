f7cd9b66b727b98a0328c6d8d886f287858b1f68
wayhong0928 <j520123456789@gmail.com>
2022-08-21 22:36:08 +0800

    function practice

--- onlineteach/python/backup/function-basic.py
+++ onlineteach/python/backup/function-basic.py
@@ -1,8 +1,8 @@
 # 定義凾式
 # 凾式內部的程式碼如果沒有做凾式呼叫就不會執行
 
-def multiply(x,y):
-    
+def multiply(x, y):
+
     return x*y
 
 # 呼叫凾式
@@ -12,10 +12,12 @@ def multiply(x,y):
 
 # 凾式的包裝
 
+
 def calculate(max):
-    sum=0
-    for n in range(1,max+1):
-        sum+=n
+    sum = 0
+    for n in range(1, max+1):
+        sum += n
     print(sum)
+
 
 calculate(int(input("input:")))
