a281e6dd380b45a5120892d46ba9966a45aa88f1
wayhong0928 <j520123456789@gmail.com>
2022-08-21 22:38:03 +0800

    Flask start

--- onlineteach/Flask/app.py
+++ onlineteach/Flask/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
-app=Flask(__name__) # 建立 Application 物件
+app = Flask(__name__)  # 建立 Application 物件
 
 # 建立網站首頁的回應方式
+
+
 @app.route("/")
 def index():              # 用來回應網站首頁連線的函式
     return "Hello Flask"  # 回傳網站首頁的內容
+
 
 # 啟動網站伺服器
 app.run()
