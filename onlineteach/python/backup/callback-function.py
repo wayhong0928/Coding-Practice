9b70ecdd4d9397e6c2f58694a187592ada03e5f4
wayhong0928 <j520123456789@gmail.com>
2022-08-21 22:35:15 +0800

    callback_function

--- onlineteach/python/backup/callback-function.py
+++ onlineteach/python/backup/callback-function.py
@@ -2,17 +2,23 @@ def test(arg):
     arg("hello")   # 呼叫回呼函式
 
 # 定義一個回呼函式
+
+
 def handle(da):
     print(da)
+
 
 test(handle)
 
 #-----------------------------#
 
-def add(n1,n2,cb):
+
+def add(n1, n2, cb):
     cb(n1+n2)
 
-def handle1(result):
-    print("ans",result)
 
-add(3,4,handle1)
+def handle1(result):
+    print("ans", result)
+
+
+add(3, 4, handle1)
