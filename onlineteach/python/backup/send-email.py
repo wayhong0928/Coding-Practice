18f64495e8a61f95bb5aaebc5068309760af33b7
wayhong0928 <j520123456789@gmail.com>
2022-08-23 15:08:04 +0800

    sned email

--- onlineteach/python/backup/send-email.py
+++ onlineteach/python/backup/send-email.py
@@ -1,11 +1,12 @@
 # 寄送 Email 程式
 # 準備訊息物件設定
 
+import smtplib
 import email.message
-msg=email.message.EmailMessage()
-msg["From"]="714316@clvsc.tyc.edu.tw"
-msg["To"]="714316@clvsc.tyc.edu.tw"
-msg["Subject"]="hello world"
+msg = email.message.EmailMessage()
+msg["From"] = "Email"
+msg["To"] = "Email"
+msg["Subject"] = "hello world"
 
 # 寄送純文字的內容
 
@@ -17,12 +18,11 @@ msg.add_alternative("<h3>CLVSC</h3>is a ..ya you know", subtype="html")
 
 # 連線到 SMTP Server，驗證寄件人身份並發送郵件
 
-import smtplib
 
 # 到網路上搜尋 gmail smtp server 或是 yahoo smtp sercer
 
-server=smtplib.SMTP_SSL("smtp.gmail.com", 465)
+server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
 # password
-server.login("714316@clvsc.tyc.edu.tw", "password")
+server.login("Email", "password")
 server.send_message(msg)
 server.close()
